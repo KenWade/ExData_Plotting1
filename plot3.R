e1b37a5a989b3ce7de9c91dc6bdf4fd7ad93780c
Ken Wade <kenwade@umich.edu>
2015-12-11 00:33:37 +0700

    Plot 3 Complete

---
+++ plot3.R
@@ -0,0 +1,89 @@
+####
+#
+#   plot3.R
+#
+#   Exploratory Data Analysis (class.coursera.org/exdata-035)
+#
+#   Making Plots
+#
+#       Our overall goal here is simply to examine how household energy usage varies over a 2-day period in
+#       February, 2007. Your task is to reconstruct the following plots below, all of which were constructed using
+#       the base plotting system.
+#
+
+####
+#
+#   Setup Libraries
+#
+
+library(lubridate)
+
+####
+#
+#    Read in the Electric power consumption data from the working directory
+#
+#       As a side detail suggusted in the project description...
+#           find the size of the data frame object and consider the implecations.
+#
+#               Input file size:        132,960,755 bytes ~ 130MB
+#               Data Frame object.size: 149,581,752 bytes ~ 142MB
+#               Data Frame dimensions:  2075259 obs. of  9 variables
+#                   7.12 bytes per variable per observation in the data file
+#                   8.00 bytes per variable per observation in the data frame
+#
+# Data daken from UCI Machine Learning Repository:
+#                   https://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption
+#
+# Downloaded from: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+get_household_power_consumption_data <- function() {
+
+    hpc_filename   <- "household_power_consumption.txt"
+    hpc_colClasses <- c("character",  "character", "numeric", "numeric", "numeric",
+                        "numeric", "numeric", "numeric", "numeric")
+    hpc_nastrings  <- "?"
+
+    minDate <- as.Date("2007-02-01", "%Y-%m-%d")
+    maxDate <- as.Date("2007-02-02", "%Y-%m-%d")
+
+    epcFull <- read.table(hpc_filename, header=TRUE, sep=";", colClasses = hpc_colClasses, na.strings = hpc_nastrings)
+
+    hpc_filesize   <- file.size(hpc_filename)
+    hpc_objectsize <- object.size(epcFull)
+    bytes_per_variable_per_observation_in_datafile <- hpc_filesize / ncol(epcFull) / nrow(epcFull)
+    bytes_per_variable_per_observation_in_object   <- hpc_objectsize / ncol(epcFull) / nrow(epcFull)
+    
+####
+#
+#   Convert date and time to R date and time classes and
+#
+
+    epcFull$DateTime <- strptime(paste(epcFull$Date, epcFull$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+
+####
+#
+#   Select only the dates of interest
+#
+
+    epc <- subset(epcFull, (as.Date(epcFull$DateTime) >= minDate) & (as.Date(epcFull$DateTime) <= maxDate))
+
+    return(epc)
+}
+
+#######################################################################
+#
+# Make Plot3
+#
+#######################################################################
+
+epc <- get_household_power_consumption_data()
+
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+plot(epc$DateTime,  epc$Sub_metering_1, type = "l", col = "black", lwd = 1, xlab = "", ylab = "Energy sub metering")
+lines(epc$DateTime, epc$Sub_metering_2, type = "l", col = "red",   lwd = 1)
+lines(epc$DateTime, epc$Sub_metering_3, type = "l", col = "blue",  lwd = 1)
+legend("topright", lty=c(1,1, 1), lwd=c(1, 1, 1), col=c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
+
+
